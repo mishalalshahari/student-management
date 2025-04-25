470f14538b0be8e3cd102fb4a0ad3b45e1234cda
Mishal Al Shahari <mishalalshahari@gmail.com>
2025-04-25 21:00:09 +0530

    configured swagger open api ui and doc

--- src/main/java/com/studentmanagement/Repository/StudentRepository.java
+++ src/main/java/com/studentmanagement/Repository/StudentRepository.java
@@ -14,17 +14,20 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
 
     @Query("select name from Student where name = :name")
     List<Student> findByName(@Param("name") String name);
+    
+    List<Student> findByNameIgnoreCase(String name);
 
     List<Student> findByAgeGreaterThan(int age);
 
     List<Student> findByNameStartingWith(String prefix);
 
-    List<Student> findByNameContainingIgnoreCase(String keyword);
+    List<Student> findByNameStartingWithIgnoreCase(String prefix);
 
+    List<Student> findByNameContainingIgnoreCase(String keyword);
+    
     List<Student> findAllByOrderByNameAsc();
     
-    // Custom query methods
-    List<Student> findByNameStartingWithIgnoreCase(String prefix);
     
-    List<Student> findByNameIgnoreCase(String name);
+    
+    
 }
